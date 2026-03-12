6cc7424e3bdfbc35cda2b72bfd2e5c4d355e532a
Pratibha Shekhawat <shekhawatzinnu@gmail.com>
2026-03-12 16:14:38 +0530

    commit

---
+++ Practice/day1task3.py
@@ -0,0 +1,18 @@
+#task3
+# from time import sleep
+# from selenium.webdriver import Chrome, ChromeOptions
+# o=ChromeOptions()
+# o.add_experimental_option('detach',True)
+# driver = Chrome(options=o)
+# driver.get('https://www.wikepedia.com')
+# sleep(3)
+# driver.refresh()
+# sleep(4)
+# print(driver.title)
+# driver.get('https://www.amazon.com')
+#
+# sleep(3)
+# print(driver.title)
+# driver.back()
+# sleep(3)
+# driver.close()
---
+++ class/launch.py
@@ -0,0 +1,78 @@
+
+#launchung the browser
+# from time import sleep
+# from selenium.webdriver import Chrome
+# driver= Chrome()
+# sleep(5)
+
+# from selenium.webdriver import Chrome,ChromeOptions
+# o=ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+
+#to connect to website and open a url
+# from selenium.webdriver import Chrome,ChromeOptions
+# o=ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# #to open a webpage
+# driver.get("https://www.google.com")
+# #to maximize
+# driver.maximize_window()
+
+
+from time import sleep
+# from selenium.webdriver import Chrome,ChromeOptions
+# o=ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# driver.get("https://www.google.com")
+# driver.maximize_window()
+# sleep(4)
+# driver.fullscreen_window()
+# #driver.maximize_window()
+
+#to fetch the title
+# title= driver.title
+# print(title)
+
+# from time import sleep
+# from selenium.webdriver import Chrome,ChromeOptions
+# o=ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# driver.get("https://demoqa.com/")
+# print(driver.title)
+# print(driver.current_url)
+# sleep(4)
+# driver.close()
+
+# from time import sleep
+# from selenium.webdriver import Chrome,ChromeOptions
+# o=ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# driver.get("https://wikepedia.com/")
+# driver.maximize_window()
+# print(driver.title)
+# print(driver.current_url)
+# sleep(4)
+# driver.back()
+# sleep(4)
+# driver.forward()
+# sleep(4)
+# driver.refresh()
+# sleep(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
